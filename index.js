9742a3ab7a961ac638e0dfb91afc755cc7f17ef5
Prateek gupta <gprateek558@gmail.com>
2020-05-23 12:29:44 +0530

    setup server

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const port = 8000;
+const app = express();
+
+
+
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log("error");
+        return;
+    }
+
+    console.log("server running on port"+port);
+
+});
+
+
